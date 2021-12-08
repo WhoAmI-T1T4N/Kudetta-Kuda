d69bb326b68a45be38590f18b2f4711e4d66296c
Males Aja Kasih Nama :V <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-12-08 18:38:17 +0700

    Create HackOntak.sh

---
+++ HackOntak.sh
@@ -0,0 +1,140 @@
+# Halahh Kang Recode Aja
+# Bilang Ke Authornya Nyett
+#!/bin/category/bash
+
+gn='\033[0;32m'
+gl='\033[32;1m'
+rd='\033[31;1m'
+we='\033[37;1m'
+bk='\033[30;1m'
+yw='\033[33;1m'
+yl='\033[1;33m'
+be='\033[36;1m'
+bl='\033[0;36m'
+
+# tampilan
+clear
+echo -e $rd "██╗  ██╗ ██████╗ ███╗   ██╗████████╗ █████╗ ██╗  ██╗"
+echo -e $rd "██║ ██╔╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║ ██╔╝"
+echo -e $rd "█████╔╝ ██║   ██║██╔██╗ ██║   ██║   ███████║█████╔╝" 
+echo -e $we "██╔═██╗ ██║   ██║██║╚██╗██║   ██║   ██╔══██║██╔═██╗" 
+echo -e $we "██║  ██╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║  ██╗"
+echo -e $we "╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝"
+echo -e
+    echo -e $bk "×××××××××××××××××××××××××"
+    echo -e $yl "Author : Mr.T1T4N"
+    echo -e $yl "Team : MBT | LCT | ITL"
+    echo -e $yl "YT : T1T4N TZY"
+    echo -e $yl "Dear : Lu Yang Master Ajarin Newbie"
+    echo -e $bk "××××××××××××××××××××××××××"
+echo -e
+echo -e
+   echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+   echo -e $gl "1]. Hack Kontak"
+   echo -e $gl "2]. Keluar"
+   echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+    echo -e $gn "Pilih Memek"
+        read -p "=>" lp
+if [[ $lp == 1 || $lp == 1 ]]
+then
+clear
+sleep 6
+echo -e $rd "=-------------"
+echo -e $we "10%"
+clear
+sleep
+echo -e $rd "===-----------"
+echo -e $we "35%"
+clear
+sleep 6
+echo -e $rd "======--------"
+echo -e $we "46%"
+clear
+sleep 6
+echo -e $rd "========------"
+echo -e $we "67%"
+clear
+sleep 6
+echo -e $rd "===========---"
+echo -e $we "79%"
+clear
+sleep 6
+echo -e $rd "=============="
+echo -e $we "100%"
+clear
+sleep 6
+echo -e $gl "SUCCESFULLY, SEDANG MENYIAPKAN TARGET"
+sleep 2
+echo -e $we "EITSSS..."
+sleep 2
+echo -e $rd "Sepertinya Kau Belum Menginstall Bahannya"
+sleep 3
+pip2 install requests
+pkg install python2
+pip2 install mechanize
+pip install bs4
+sleep 2
+echo -e $rd "Sipp, Sepertinya Sudah Di Install"
+echo -e $we "Sedang Menunju, Ke Target"
+clear
+echo -e $rd "██╗  ██╗ █████╗  ██████╗██╗  ██╗"
+echo -e $rd "██║  ██║██╔══██╗██╔════╝██║ ██╔╝"
+echo -e $rd "███████║███████║██║     █████╔╝ "
+echo -e $we "██╔══██║██╔══██║██║     ██╔═██╗"
+echo -e $we "██║  ██║██║  ██║╚██████╗██║  ██╗"
+echo -e $we "╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝"
+echo -e
+echo -e
+echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+echo -e $gl "1]. Cowo Orang"
+echo -e $gl "2]. Cewe Orang"
+echo -e $bk "∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+echo -e $yl "Pilih Ngab"
+  read -p "=>" pk
+sleep 0.1
+else
+sleep 2
+echo -e $bk "SALAHKAN ASU"
+sleep 3
+bash HackOnta.sh
+fi
+if [[ $lp == 2 || $lp == 2 ]]
+then
+echo -e $rd "██████╗  █████╗ ██████╗  █████╗ ██╗   ██╗"
+echo -e $rd "██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝"
+echo -e $rd "██████╔╝███████║██████╔╝███████║ ╚████╔╝"
+echo -e $we "██╔══██╗██╔══██║██╔══██╗██╔══██║  ╚██╔╝"  
+echo -e $we "██████╔╝██║  ██║██████╔╝██║  ██║   ██║"   
+echo -e $we "╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝" 
+sleep 0.1
+else
+sleep 2
+echo -e $bk "Hadehh Gak NGOTAK"
+sleep 3
+bash HackOnta.sh
+fi                                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
